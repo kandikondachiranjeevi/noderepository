637b7e6708f50ea623a881b566439f87c3a1a51b
chiranjeevi <kandikondachiranjeevi24@gmail.com>
2023-04-26 13:12:33 +0000

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const addDays = require("date-fns/addDays");
 
-const result = addDays(new Date(2021, 0, 20), 3);
+const resul = addDays(new Date(2021, 0, 20), 3);
 
-console.log(result);
+console.log(resul);
